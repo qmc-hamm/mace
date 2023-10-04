bf21ba2c698b6545cc0913f45ec1770866152380
Shriram <shriramathreya786@gmail.com>
2023-10-04 18:25:41 -0500

    add dataset in example

--- calc_test.py
+++ calc_test.py
@@ -11,12 +11,12 @@ model_uri = f"runs:/{run_id}/model/"
 device = "cpu"
 
 calculator = MACECalculator(model_path=model_uri + device, device=device)
-init_conf = read('qmc/testing.xyz', '0')
+init_conf = read('BOTNet-datasets/dataset_3BPA/test_300K.xyz', '0')
 init_conf.set_calculator(calculator)
 
 dyn = Langevin(init_conf, 0.5*units.fs, temperature_K=310, friction=5e-3)
 def write_frame():
-        dyn.atoms.write('output.xyz', append=True)
+        dyn.atoms.write('md_3bpa.xyz', append=True)
 dyn.attach(write_frame, interval=50)
 dyn.run(100)
 print("MD finished!")
