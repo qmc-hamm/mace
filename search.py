0777b6a3682c76540d9735e80e81c42b1038138d
Ben Galewsky <ben@peartreestudio.net>
2024-04-26 10:12:45 -0500

    Make max_num_epochs an argument

--- search.py
+++ search.py
@@ -9,12 +9,16 @@ from mlflow.tracking import MlflowClient
 tracking_client = mlflow.tracking.MlflowClient()
 
 
-def run_train(experiment_id, r_max, forces_weight, energy_weight, train_file, valid_file, backend_config="slurm_config.json", parent_run_id=None):
+def run_train(experiment_id, r_max, forces_weight, energy_weight, max_num_epochs,
+    train_file, valid_file,
+    backend_config="slurm_config.json",
+    parent_run_id=None):
     p = mlflow.projects.run(
         uri=os.path.dirname(os.path.realpath(__file__)),
         entry_point="main",
         parameters={
             "r_max": float(r_max),
+            "max_num_epochs": int(max_num_epochs),
             "forces_weight": float(forces_weight),
             "energy_weight": float(energy_weight),
             "train_file": str(train_file),
@@ -26,7 +30,10 @@ def run_train(experiment_id, r_max, forces_weight, energy_weight, train_file, va
         backend_config=backend_config
     )
     tracking_client.log_batch(run_id=p.run_id, metrics=[],
-                             params=[Param("r_max", str(r_max)), Param("forces_weight", str(forces_weight)), Param("energy_weight", str(energy_weight))],
+                             params=[Param("r_max", str(r_max)),
+                                 Param("max_num_epochs", str(max_num_epochs)),
+                                 Param("forces_weight", str(forces_weight)),
+                                 Param("energy_weight", str(energy_weight))],
                              tags=[RunTag(mlflow.utils.mlflow_tags.MLFLOW_PARENT_RUN_ID, str(parent_run_id))])
 
     return p
@@ -34,10 +41,11 @@ def run_train(experiment_id, r_max, forces_weight, energy_weight, train_file, va
 
 @click.command(help="Perform grid search over train (main entry point).")
 @click.option("--num_runs", type=click.INT, default=2, help="Maximum number of runs to evaluate.")
+@click.option("--max_num_epochs", type=click.INT, default=100, help="Maximum number of epochs to train.")
 @click.option("--train_backend_config", type=click.STRING, default="slurm_config.json", help="Json file for training jobs")
 @click.option("--train_file", type=click.STRING, default="qmc/training.xyz", help="Training File Path")
 @click.option("--valid_file", type=click.STRING, default="qmc/testing.xyz", help="Testing File Path")
-def run(num_runs, train_backend_config, train_file, valid_file):
+def run(num_runs, max_num_epochs,train_backend_config, train_file, valid_file):
     provided_run_id = os.environ.get("MLFLOW_RUN_ID", None)
     with mlflow.start_run(run_id=provided_run_id) as run:
         if not provided_run_id:
@@ -49,11 +57,13 @@ def run(num_runs, train_backend_config, train_file, valid_file):
         for r_max, forces_weight in runs:
             jobs.append(run_train(
                 experiment_id,
-                r_max=r_max, forces_weight=forces_weight, energy_weight=1.0 ,train_file=train_file,
+                r_max=r_max, forces_weight=forces_weight, energy_weight=1.0,
+                max_num_epochs=max_num_epochs,
+                train_file=train_file,
                 valid_file=valid_file, backend_config=train_backend_config,
                 parent_run_id=provided_run_id)
             )
-        
+
         print("Launched Jobs: ", jobs)
         print("Started %d slurm jobs" % len(jobs))
 
